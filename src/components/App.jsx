0de60a6715fd18d20112c1534a89b37e350d7c32
Sergii Tatianchenko <taurus090@gmail.com>
2023-07-26 16:12:50 +0300

    fixed app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react"
 import { nanoid } from "nanoid";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -8,109 +8,106 @@ import List from "./list/List";
 
 import s from "./app.module.css";
 import Message from "./message/Message";
-
-
-
-class App extends Component {
-  state = {
-    contacts: [
-      {
-        id:'1',
-        name:"John",
-        phone: '123'
-      },
-      {
-        id:'21',
-        name:"John Smith",
-        phone: '123'
-      },
-      {
-        id:'3',
-        name:"Bob",
-        phone: '123'
-      },
-    ],
-    filter: ''
-  }
-  componentDidMount() {
-    let contacts = JSON.parse(localStorage.getItem('contact'))
-    if (contacts) {
-      this.setState({
-        ...this.state,
-        contacts: [
-          ...contacts
-        ]
-      })
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState !== this.state)
-    localStorage.setItem("contact", JSON.stringify(this.state.contacts));
-  }
-  deleteContact = (idCandidate) => {
-    this.setState(prevState => ({
-        contacts: prevState.contacts.filter(contact => contact.id !== idCandidate)
-     })
+const App = () => {
+    const [state,setState] = useState({
+            contacts: [
+              {
+                id:'1',
+                name:"John",
+                phone: '123'
+              },
+              {
+                id:'21',
+                name:"John Smith",
+                phone: '123'
+              },
+              {
+                id:'3',
+                name:"Bob",
+                phone: '123'
+              },
+            ],
+            filter: ''
+    })
+    useEffect(()=>{
+        const contacts = JSON.parse(localStorage.getItem('contact'))
+        if (contacts) {
+          setState({
+            ...state,
+            contacts: [
+              ...contacts
+            ]
+          })
+        }
+    }, [state])
+    
+  const deleteContact = (idCandidate) => {
+    setState({
+        ...state,
+        contacts: state.contacts.filter(contact => contact.id !== idCandidate)
+     }
     )
   }
-  changeFilterValue = (newValue) => { 
-    this.setState(prevState => ({
+  const changeFilterValue = (newValue) => { 
+    setState({
+        ...state,
         filter: newValue
       })
-    )
   }
   
-  setNewContact = (contactCandidate) => { 
+  const setNewContact = (contactCandidate) => { 
     console.log(contactCandidate)
-      const result = this.state.contacts.find(contactItem => contactItem.name.toLowerCase() === contactCandidate.name.toLowerCase())
+      const result = state.contacts.find(contactItem => contactItem.name.toLowerCase() === contactCandidate.name.toLowerCase())
       if (result){
         Notify.warning(`${contactCandidate.name} is already in contact`)
         return ({})
       } 
-      this.setState(prevState => ({
+      setState({
+            ...state,
             contacts: [
-              ...prevState.contacts,
+              ...state.contacts,
               {
                 id: nanoid(),
                 ...contactCandidate
               }
             ]
-          })
+          }
         )
     
   }
-  startSearch = () => {
-    const {contacts, filter}= this.state
+  const startSearch = () => {
+    const {contacts, filter}= state
         const result = contacts.filter(contactItem => {
         return contactItem.name.toLowerCase().includes(filter.toLowerCase())
         })
         return result
   }
-  render() {
+
     return (
       <div>
       <h2 className={s.title}>Phonebook</h2>
       <Form 
-        setNewContact = {this.setNewContact}
+        setNewContact = {setNewContact}
       />
       <Filter
-        filterValue = {this.state.filter} 
-        changeEvent = {this.changeFilterValue}
+        filterValue = {state.filter} 
+        changeEvent = {changeFilterValue}
         />
         {
           
-          this.state.contacts.length === 0 ||
-          (this.state.filter.length !== 0 &&  this.startSearch().length === 0) ?
+          state.contacts.length === 0 ||
+          (state.filter.length !== 0 &&  startSearch().length === 0) ?
           <Message text="No items in list"/> : 
           <List 
-            list={this.startSearch()}
-            deleteContact={this.deleteContact} 
+            list={startSearch()}
+            deleteContact={deleteContact} 
           />
         }
       
 
       </div>
     )
-  }
+  
 }
+
 export default App
