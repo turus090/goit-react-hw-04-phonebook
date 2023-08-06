65589d23f2162803f95c849e3e9e41d7f975ca4b
Sergii Tatianchenko <taurus090@gmail.com>
2023-08-06 10:13:54 +0300

    fixed App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,68 @@
-import { useState, useEffect } from "react"
-import { nanoid } from "nanoid";
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-import Filter from "./filter/Filter"; 
-import Form from "./form/Form";
-import List from "./list/List";
+import Filter from './filter/Filter';
+import Form from './form/Form';
+import List from './list/List';
 
-import s from "./app.module.css";
-import Message from "./message/Message";
+import s from './app.module.css';
+import Message from './message/Message';
 const App = () => {
-  const uploadLocalContacts = () => {
-    if (localStorage.getItem('contact')){
-      return  JSON.parse(localStorage.getItem('contact'))
-    } else {
-      return []
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contact')) || []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contact', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = idCandidate => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== idCandidate)
+    );
+  };
+  const changeFilterValue = newValue => {
+    setFilter(newValue);
+  };
+
+  const setNewContact = contactCandidate => {
+    console.log(contactCandidate);
+    const result = contacts.find(
+      contactItem =>
+        contactItem.name.toLowerCase() === contactCandidate.name.toLowerCase()
+    );
+    if (result) {
+      Notify.warning(`${contactCandidate.name} is already in contact`);
+      return {};
     }
-   
-  }
-    const [contacts, setContacts] = useState(uploadLocalContacts())
-    const [filter, setFilter] = useState('')
-    
-    useEffect(()=>{
-      localStorage.setItem('contact', JSON.stringify(contacts))
-    },[contacts])
+    setContacts(prevState => [
+      ...prevState,
+      {
+        id: nanoid(),
+        ...contactCandidate,
+      },
+    ]);
+  };
+  const startSearch = () =>
+    contacts.filter(contactItem => {
+      return contactItem.name.toLowerCase().includes(filter.toLowerCase());
+    });
 
-  const deleteContact = (idCandidate) => {
-      setContacts(prevState=>(
-        prevState.filter(contact => contact.id !== idCandidate)
-      ))
-  }
-  const changeFilterValue = (newValue) => { 
-   setFilter(newValue)
-  }
-  
-  const setNewContact = (contactCandidate) => { 
-    console.log(contactCandidate)
-      const result = contacts.find(contactItem => contactItem.name.toLowerCase() === contactCandidate.name.toLowerCase())
-      if (result){
-        Notify.warning(`${contactCandidate.name} is already in contact`)
-        return ({})
-      } 
-      setContacts(prevState => ([
-        ...prevState,
-        {
-          id: nanoid(),
-          ...contactCandidate
-        }
-      ]))
-  }
-  const startSearch = () => (contacts.filter(contactItem => {
-          return contactItem.name.toLowerCase().includes(filter.toLowerCase())
-          })
-        )
-
-    return (
-      <div>
+  return (
+    <div>
       <h2 className={s.title}>Phonebook</h2>
-      <Form 
-        setNewContact = {setNewContact}
-      />
-      <Filter
-        filterValue = {filter} 
-        changeEvent = {changeFilterValue}
-        />
-        {
-          
-          contacts.length === 0 ||
-          (filter.length !== 0 &&  startSearch().length === 0) ?
-          <Message text="No items in list"/> : 
-          <List 
-            list={startSearch()}
-            deleteContact={deleteContact} 
-          />
-        }
-      
+      <Form setNewContact={setNewContact} />
+      <Filter filterValue={filter} changeEvent={changeFilterValue} />
+      {contacts.length === 0 ||
+      (filter.length !== 0 && startSearch().length === 0) ? (
+        <Message text="No items in list" />
+      ) : (
+        <List list={startSearch()} deleteContact={deleteContact} />
+      )}
+    </div>
+  );
+};
 
-      </div>
-    )
-  
-}
-
-export default App
+export default App;
